6422536e541a5616dc720c113da3e153046c543d
abdoullahihassan0-byte <abdoullahihassan0@gmail.com>
2026-08-13 23:27:31 +0100

    Create db.js

---
+++ mcf-backend/src/config/db.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('error', (err) => {
+  console.error('Erreur inattendue sur le pool PostgreSQL', err);
+  process.exit(1);
+});
+
+module.exports = pool;
